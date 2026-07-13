9116aa987692e814aa096ebf0f9bbc4a557ac981
Matthew Gabiou <mgabiou@proton.me>
2026-07-13 11:37:58 -0500

    Returning child resources

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -6,26 +6,28 @@ namespace CityInfo.API.Controllers
     [ApiController]
     [Route("api/cities")]
     public class CitiesController : Controller
-    {
-        
+    {    
         [HttpGet]     
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public ActionResult<IEnumerable<CityDto>> GetCities() // Choose the model (CityDto)
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            return Ok(CitiesDataStore.Current.Cities); // Get an instance of the datastore
         }
 
         [HttpGet("{id}")]
-        public ActionResult<CityDto> GetCity(int id)
+        public ActionResult<CityDto> GetCity(int id) // Use the CityDto model
         {
-            // find city
+            // find specific city
             var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
 
+
+            // Return types of http status codes
+            // No specific city found
             if (cityToReturn == null)
             {
-                return NotFound();
+                return NotFound(); // Return 404
             }
-            
-            return Ok(cityToReturn);
+            // else return 200
+            return Ok(cityToReturn); // Return 200
         }
     }
 }
--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -6,6 +6,17 @@
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
 
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
 
+
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+            = new List<PointOfInterestDto>();
     }
 }
