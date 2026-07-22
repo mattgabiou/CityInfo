f47b46a0bd2cf0e5b687a2579b0956de4ec7460b
Matt Gabiou <mgabiou@proton.me>
2026-07-22 00:52:30 -0500

    Create Point Of Interest [Incomplete]

---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string? Descritpion {  get; set; }
+    }
+}
