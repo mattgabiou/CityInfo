f8191522264f1ea47a0787a3247fbd10764fda23
Matthew Gabiou <mgabiou@proton.me>
2026-07-26 16:36:14 -0500

    Full update example

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -80,5 +80,30 @@ namespace CityInfo.API.Controllers
                  },
                  finalPointOfInterest);
         }
+            
+        [HttpPut("{pointofinterestid}")]
+        public ActionResult UpdatePointOfInterest(int cityId, int  pointOfInterestId, 
+            PointOfInterestForUpdateDto pointOfInterest)        
+        {
+            // We check to see if we can find the point of interest to update. If not, we return not found
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // find point of interest
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(c=> c.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
     }
 }
